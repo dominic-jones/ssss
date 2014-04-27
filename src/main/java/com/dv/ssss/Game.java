b36ab33e95201f8c200e713f54f5b68c986ca96a
Dominic Jones <dominic.jones@elasticpath.com>
2014-04-27 11:26:39 -0700

    Simple spike to see how java fx works

---
+++ src/main/java/com/dv/ssss/Game.java
@@ -0,0 +1,30 @@
+package com.dv.ssss;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Game extends Application {
+
+    public static void main(String[] args) {
+
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        primaryStage.setTitle("SSSS");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+
+        btn.setOnAction(event -> System.out.println("Hello World"));
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
+}
