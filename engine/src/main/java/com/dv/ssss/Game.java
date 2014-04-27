5b21935fb345c916df03347cadc5a4c5ca8bbf9f
Dominic Jones <dominic.jones@elasticpath.com>
2014-04-27 15:10:48 -0700

    Reworking layout, adding a dummy turn track

--- engine/src/main/java/com/dv/ssss/Game.java
+++ engine/src/main/java/com/dv/ssss/Game.java
@@ -6,12 +6,18 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Game extends Application {
 
+    public static final int SPACING = 5;
+    public static final Insets INSETS = new Insets(10, 0, 0, 10);
+    public static final Font FONT = new Font("Arial", 20);
     private PersonnelService personnelService = new PersonnelService();
 
     public static void main(String[] args) {
@@ -23,25 +29,47 @@ public class Game extends Application {
     public void start(Stage stage) {
 
         Scene scene = new Scene(new Group());
-        stage.setTitle("Personnel");
+        stage.setTitle("SSSS");
         stage.setWidth(300);
         stage.setHeight(500);
 
-        final Label label = new Label("Personnel");
-        label.setFont(new Font("Arial", 20));
+        HBox turn = turnTrack();
+
+        VBox personnel = personnel();
+
+        BorderPane layout = new BorderPane();
+        layout.setTop(turn);
+        layout.setCenter(personnel);
+
+        ((Group) scene.getRoot()).getChildren().addAll(layout);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private HBox turnTrack() {
+
+        HBox turn = new HBox();
+        turn.setSpacing(SPACING);
+        turn.setPadding(INSETS);
+        Label turnLabel = new Label("Turn");
+        Text turnCount = new Text("1");
+        turn.getChildren().addAll(turnLabel, turnCount);
+        return turn;
+    }
+
+    private VBox personnel() {
+
+        Label label = new Label("Personnel");
+        label.setFont(FONT);
 
         TableView<Person> table = new AnnotatedTable().createTable(personnelService.get(), Person.class);
         table.setEditable(false);
 
-        final VBox vbox = new VBox();
-        vbox.setSpacing(5);
-        vbox.setPadding(new Insets(10, 0, 0, 10));
+        VBox vbox = new VBox();
+        vbox.setSpacing(SPACING);
+        vbox.setPadding(INSETS);
         vbox.getChildren().addAll(label, table);
-
-        ((Group) scene.getRoot()).getChildren().addAll(vbox);
-
-        stage.setScene(scene);
-        stage.show();
+        return vbox;
     }
 
 }
