ea4a5ac8aaa3052854705bceb3ea1dc5db6f510f
Dominic Jones <dominic.jones@elasticpath.com>
2014-04-27 16:20:05 -0700

    Pushing UI specific components down, leaving Game for bootstrapping

--- engine/src/main/java/com/dv/ssss/Game.java
+++ engine/src/main/java/com/dv/ssss/Game.java
@@ -1,30 +1,11 @@
 package com.dv.ssss;
 
-import com.dv.ssss.people.Person;
-import com.dv.ssss.people.PersonnelService;
-import com.dv.ssss.ui.AnnotatedTable;
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Game extends Application {
 
-    public static final int SPACING = 5;
-    public static final Insets INSETS = new Insets(10, 0, 0, 10);
-    public static final Font FONT = new Font("Arial", 20);
-
-    private Engine engine = new Engine();
-    private PersonnelService personnelService = new PersonnelService();
+    private UI ui;
 
     public static void main(String[] args) {
 
@@ -34,54 +15,7 @@ public class Game extends Application {
     @Override
     public void start(Stage stage) {
 
-        Group group = new Group();
-        Scene scene = new Scene(group);
-        stage.setTitle("SSSS");
-        stage.setWidth(300);
-        stage.setHeight(500);
-
-        HBox turn = turnTrack();
-
-        VBox personnel = personnel();
-
-        BorderPane layout = new BorderPane();
-        layout.setTop(turn);
-        layout.setCenter(personnel);
-
-        group.getChildren().addAll(layout);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    private HBox turnTrack() {
-
-        Label label = new Label("Turn");
-
-        Text turnCount = new Text("1");
-
-        Button endTurn = new Button("End Turn");
-        endTurn.setOnAction(event -> engine.endTurn());
-
-        HBox turn = new HBox();
-        turn.setSpacing(SPACING);
-        turn.setPadding(INSETS);
-        turn.getChildren().addAll(label, turnCount, endTurn);
-        return turn;
-    }
-
-    private VBox personnel() {
-
-        Label label = new Label("Personnel");
-        label.setFont(FONT);
-
-        TableView<Person> table = new AnnotatedTable().createTable(personnelService.get(), Person.class);
-        table.setEditable(false);
-
-        VBox vbox = new VBox();
-        vbox.setSpacing(SPACING);
-        vbox.setPadding(INSETS);
-        vbox.getChildren().addAll(label, table);
-        return vbox;
+        ui.display(stage);
     }
 
 }
