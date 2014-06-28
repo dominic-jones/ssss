ff7802b9015d9f2b8f4d1e70106323ffaea5db8c
Dominic Jones <dominic.jones@elasticpath.com>
2014-06-28 12:56:44 -0700

    Renaming / collapsing mixin

--- engine/src/main/java/com/dv/ssss/DataBootstrap.java
+++ engine/src/main/java/com/dv/ssss/DataBootstrap.java
@@ -1,9 +1,43 @@
 package com.dv.ssss;
 
+import com.dv.ssss.people.PersonFactory;
+import com.dv.ssss.turn.TurnFactory;
+import org.qi4j.api.injection.scope.Service;
+import org.qi4j.api.injection.scope.Structure;
 import org.qi4j.api.mixin.Mixins;
+import org.qi4j.api.unitofwork.UnitOfWork;
+import org.qi4j.api.unitofwork.UnitOfWorkCompletionException;
+import org.qi4j.api.unitofwork.UnitOfWorkFactory;
 
-@Mixins(DataBootstrapImpl.class)
+@Mixins(DataBootstrap.DataBootstrapMixin.class)
 public interface DataBootstrap {
 
     void bootstrap();
+
+    class DataBootstrapMixin implements DataBootstrap {
+
+        @Service
+        PersonFactory personFactory;
+
+        @Service
+        TurnFactory turnFactory;
+
+        @Structure
+        UnitOfWorkFactory unitOfWorkFactory;
+
+        @Override
+        public void bootstrap() {
+
+            UnitOfWork unitOfWork = unitOfWorkFactory.newUnitOfWork();
+
+            personFactory.create("Aegis", "Overlord", 23);
+            turnFactory.create();
+
+            try {
+                unitOfWork.complete();
+            } catch (UnitOfWorkCompletionException e) {
+                throw new IllegalArgumentException(e.getMessage(), e);
+            }
+        }
+    }
 }
