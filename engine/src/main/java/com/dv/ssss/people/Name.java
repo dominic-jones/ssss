c5f8c0fde26dd14ddbdf09ec943ac15baaa8b0d5
Dominic Jones <dominic.jones@elasticpath.com>
2014-07-01 18:39:18 -0700

    Removing unused method.

--- engine/src/main/java/com/dv/ssss/people/Name.java
+++ engine/src/main/java/com/dv/ssss/people/Name.java
@@ -1,24 +1,13 @@
 package com.dv.ssss.people;
 
-import org.qi4j.api.injection.scope.This;
 import org.qi4j.api.mixin.Mixins;
 import org.qi4j.api.property.Property;
 
 @Mixins(Name.NameMixin.class)
 public interface Name {
 
-    String getName();
-
     class NameMixin implements Name {
 
-        @This
-        NameState state;
-
-        @Override
-        public String getName() {
-
-            return state.name().get();
-        }
     }
 
     interface NameState {
