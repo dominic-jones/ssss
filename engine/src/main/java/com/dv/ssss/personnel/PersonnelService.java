5112c63869290d40abfd340d0995eed73353cff2
Dominic Jones <dominic.jones@elasticpath.com>
2014-07-19 10:44:19 -0700

    Applying UnitOfWorkConcern to PersonnelService

--- engine/src/main/java/com/dv/ssss/personnel/PersonnelService.java
+++ engine/src/main/java/com/dv/ssss/personnel/PersonnelService.java
@@ -1,22 +1,19 @@
 package com.dv.ssss.personnel;
 
-import static com.google.common.collect.Iterables.transform;
-import static com.google.common.collect.Lists.newArrayList;
-
 import com.dv.ssss.domain.game.GameService;
 import com.dv.ssss.domain.game.TurnDto;
 import com.dv.ssss.domain.people.PersonEntity;
 import com.dv.ssss.domain.people.PersonnelRepository;
-import com.dv.ssss.inf.DataException;
+import com.dv.ssss.inf.uow.UnitOfWorkConcern;
 import com.dv.ssss.ui.personnel.PersonDto;
-
+import org.qi4j.api.concern.Concerns;
 import org.qi4j.api.injection.scope.Service;
-import org.qi4j.api.injection.scope.Structure;
 import org.qi4j.api.mixin.Mixins;
-import org.qi4j.api.unitofwork.UnitOfWork;
-import org.qi4j.api.unitofwork.UnitOfWorkCompletionException;
-import org.qi4j.api.unitofwork.UnitOfWorkFactory;
 
+import static com.google.common.collect.Iterables.transform;
+import static com.google.common.collect.Lists.newArrayList;
+
+@Concerns(UnitOfWorkConcern.class)
 @Mixins(PersonnelService.PersonnelServiceMixin.class)
 public interface PersonnelService {
 
@@ -30,24 +27,13 @@ public interface PersonnelService {
         @Service
         PersonnelRepository personnelRepository;
 
-        @Structure
-        UnitOfWorkFactory unitOfWorkFactory;
-
         @Override
         public Iterable<PersonDto> all(String gameIdentity) {
-
-            UnitOfWork unitOfWork = unitOfWorkFactory.newUnitOfWork();
 
             TurnDto currentTurn = gameService.currentTurn(gameIdentity);
 
             Iterable<PersonEntity> people = personnelRepository.all("Aegis");
-            Iterable<PersonDto> personDtos = newArrayList(transform(people, p-> new PersonDto(p, currentTurn.getDate())));
-            try {
-                unitOfWork.complete();
-            } catch (UnitOfWorkCompletionException e) {
-                throw new DataException(e);
-            }
-            return personDtos;
+            return newArrayList(transform(people, p -> new PersonDto(p, currentTurn.getDate())));
         }
     }
 }
